93018ff2b83e4acc7cf4b27e636faf8510910008
YUNA-AHN <yunanash1234@gmail.com>
2023-11-30 17:31:02 +0900

    상속 실습

--- java_tutorials/src/org/opentutorials/oop/Inheritance/example1/CalculatorDemo1.java
+++ java_tutorials/src/org/opentutorials/oop/Inheritance/example1/CalculatorDemo1.java
@@ -1,5 +1,36 @@
 package org.opentutorials.oop.Inheritance.example1;
 
-public class CalculatorDemo1 {
+class Calculator {
+    int left, right;
+ 
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+ 
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+ 
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+}
 
+// 새로운 클래스 생성 : 뺄셈을 할 수 있는 계산기
+// extends : 확장, 하위 클래스에서 상위 클래스 기능을 확장
+class SubstractionableCalculator extends Calculator {
+	public void substract() {
+		System.out.println(this.left - this.right);
+	}
+}
+
+public class CalculatorDemo1 {
+	public static void main(String[] args) {
+		SubstractionableCalculator c1 =  new SubstractionableCalculator();
+		c1.setOprands(10, 20);
+		c1.sum();
+		c1.avg();
+		c1.substract();
+	}
 }
---
+++ java_tutorials/src/org/opentutorials/oop/Inheritance/example4/ConstructorDemo.java
@@ -0,0 +1,5 @@
+package org.opentutorials.oop.Inheritance.example4;
+
+public class ConstructorDemo {
+
+}
